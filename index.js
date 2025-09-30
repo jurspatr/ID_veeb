2a56211302473d033082dd94e0b151e590aaa5ff
Patrick Jurs <jurspatr@users.noreply.github.com>
2025-09-30 13:49:21 +0300

    Express.js esimesed marsruudid ja kasutajasisend

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const fs = require("fs");
+const dateEt = require("./src/dateTimeET");
+//me loome objekti, mis ongi express.js programm ja edasi kasutamegi seda
+const app = express();
+//mأ¤أ¤rame renderdajaks ejs
+app.set("view engine", "ejs");
+//mأ¤أ¤rame kasutamiseks "avaliku" kataloogi
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+	//res.send("Express.js lأ¤ks edukalt kأ¤ima!");
+	res.render("index");
+});
+
+app.get("/timenow", (req, res)=>{
+	res.render("timenow", {nowDate: dateEt.longDate(), nowWd: dateEt.weekday()});
+});
+
+app.get("/vanasonad", (req, res)=>{
+	fs.readFile("public/txt/vanasonad.txt", "utf8", (err, data)=>{
+		if(err){
+			res.render("genericlist", {heading: "Valik Eesti tuntud vanasõnasid", listData: ["Kahjuks vanasõnasid ei leidnud!"]});
+		} else {
+			let folkWisdom = data.split(";");
+			res.render("genericlist", {heading: "Valik Eesti tuntud vanasõnasid", listData: folkWisdom});
+		}
+	});
+	
+});
+
+app.get("/regvisit", (req, res)=>{
+	res.render("regvisit");
+});
+
+app.post("/regvisit", (req, res)=>{
+	console.log(req.body);
+	//avan tekstifaili kirjutamiseks sellisel moel, et kui teda pole, luuakse (parameeter "a")
+	fs.open("public/txt/visitlog.txt", "a", (err, file)=>{
+		if(err){
+			throw(err);
+		}
+		else {
+			//faili senisele sisule lisamine
+			fs.appendFile("public/txt/visitlog.txt", req.body.nameInput + "; ", (err)=>{
+				if(err){
+					throw(err);
+				}
+				else {
+					console.log("Salvestatud!");
+					res.render("regvisit");
+				}
+			});
+		}
+	});
+});
+
+app.listen(5220);
